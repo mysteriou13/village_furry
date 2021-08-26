6239aa2cff36569da8a5b333df41c3b782404321
mysteriou <elio007@hotmail.fr>
2021-08-26 21:18:41 +0200

    ajout nav menu

--- header.php
+++ header.php
@@ -16,6 +16,58 @@
 <body class>
 
   <nav>
+    <?php
+
+
+    $query = "SELECT * FROM wp_terms ";
+
+    $allcustomers = $wpdb->get_results($query);
+
+    foreach ($allcustomers as $singlecustomer ) {
+
+    $mycustomer = $wpdb->get_row($query);
+
+     $article = "SELECT * FROM wp_posts ";
+
+      $all = $wpdb->get_results($article);
+
+
+    echo '<div class = "catergorie">'.$singlecustomer->name."<div>";
+
+
+     $quer = "SELECT * FROM  wp_term_relationships  ";
+    $mycustomer = $wpdb->get_results($quer);
+
+    foreach ($mycustomer as $single ) {
+
+    $post = "SELECT * FROM wp_posts ";
+
+    $all = $wpdb->get_results($post);
+
+    foreach ($all as $sin) {
+
+    if($sin->ID == $single->object_id && $single->term_taxonomy_id  == $singlecustomer->term_id && $sin->post_status == "publish"){
+
+
+    echo "<div class ='article'>"."<a href = $sin->guid >".$sin->post_title."</a>"."</div>";
+
+    }
+
+    }
+
+
+
+
+
+    }
+
+     echo "</div>". '</div>';
+
+
+    }
+    ?>
+
+
   </nav>
 
 <div class = "container ">
