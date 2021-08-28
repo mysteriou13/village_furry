7eef327cc49d81d5f5551b17078f67e434b02645
mysteriou <elio007@hotmail.fr>
2021-08-28 07:24:57 +0200

    ajout include page dans ./index.php

--- index.php
+++ index.php
@@ -1,6 +1,19 @@
+
 <?php
 
 get_header();
+
+
+
+if(isset($_GET['page']) && !empty($_GET['page'])){
+
+$link  = htmlspecialchars($_GET['page']);
+
+ $page = $link.".php";
+
+include($page);
+
+}
 
 get_footer();
 
