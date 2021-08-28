7b7944de1c4a5f17a880f5c0e44f34e6412579cd
mysteriou <elio007@hotmail.fr>
2021-08-28 06:32:00 +0200

    ajout style css pour le nav menu dans  ./style.css

--- header.php
+++ header.php
@@ -9,11 +9,9 @@
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
       <?php wp_head()?>
-
-
 </head>
 
-<body class>
+<body>
 
   <nav>
     <?php
@@ -54,10 +52,6 @@
     }
 
     }
-
-
-
-
 
     }
 
